4a6747519f093887f536d5ed32ee0ad044d5092b
Adam Nobs <nobsadam@gmail.com>
2020-01-14 16:01:33 -0600

    Currently working on put, found out the code i pulled deleted alot of my other code, had to fix it and then git ride of all the links that were brought in as it was throwing off my code. This is the proper application if i ever need to refer back again

--- jsMovie.js
+++ jsMovie.js
@@ -2,12 +2,16 @@ var $Title = $('#Title');
 var $Genre = $('#Genre');
 var $Director = $('#Director');
 var $moviesList = $('#movies');
+var movieTemplate = $('#movie-template').html();
 var movieTemplate = ""+"<tr>"+ 
-"<td class='left'><h4> {{Title}}</h4></td>" +
-"<td><h4>{{Genre}}</h4></td>" + 
-"<td><h4>{{Director}}</h4></td>" +
-"<td><h4><button data-id='{{MovieId}}' class ='remove'>X</button></h4></td>" +
-"</tr>";
+ "<td class='left'><h4><span class='noedit Title'>{{Title}}</span><input class='edit Title'/></h4></td>" +
+ "<td><h4><span class='noedit Genre'>{{Genre}}</span><input class='edit Genre'/></h4></td>" + 
+ "<td><h4><span class='noedit Director'>{{Director}}</span><input class='edit Director'/></h4></td>" +
+ "<td><h4><button data-id='{{MovieId}}' class ='remove'>X</button></h4></td>" +
+ "<td><h4><button data-id='{{MovieId}}' class ='editMovie', 'noedit'>Edit</button></h4></td>" +
+ "<td><h4><button class ='saveEdit edit'>Save</button></h4></td>" +
+ "<td><h4><button class ='cancelEdit edit'>Cancel</button></h4></td>" +
+ "</tr>";
 function addMovie(movie){
     $moviesList.append(Mustache.render(movieTemplate, movie));
     // $moviesList.append('<tr><td class="left"><h4>' + movie.Title + '</h4></td><td><h4>' + movie.Genre + '</h4></td><td><h4>' + movie.Director + '</h4></td>')
@@ -47,10 +51,26 @@ $(function(){
     });
 
     $moviesList.delegate('.remove', 'click', function(){
+        var $tr = $(this).closest('tr');
+        var self = this;
+
         $.ajax({
             type: 'DELETE',
             url: 'https://localhost:44352/Api/Movie/' + $(this).attr('data-id'),
-        })
+            success: function(){
+                $tr.fadeOut(300, function(){
+                    $(this).remove();
+                });
+            }
+        });
+    });
+
+    $orders.delegate('.editMovie', 'click', function(){
+        var $tr = $(this).attr('data-id')
+        $tr.find('input.title').val($tr.find('span.title').html());
+        $tr.find('input.genre').val($tr.find('span.genre').html());
+        $tr.find('input.director').val($tr.find('span.director').html());
+        $tr.addClass('edit');
     })
 
 });
