bd8d479dbe204562f8c30a6de490c379c4f932ee
Adam Nobs <nobsadam@gmail.com>
2020-01-14 09:51:37 -0600

    Add the ajax function of get and it is working creating new tables on the frontend

--- jsMovie.js
+++ jsMovie.js
@@ -1,9 +1,14 @@
-$(function(){
-    $.ajax({
-        type: 'Get',
-        url: 'https://localhost:44352/Api/Movie',
-        success: function(data){
-            console.log('success', data);
-        }
-    });
-});
+ $(function(){
+     var $moviesList = $('#movies');
+
+     $.ajax({
+         type: 'Get',
+         url: 'https://localhost:44352/Api/Movie',
+         success: function(movies){
+             console.log('success', movies);
+                $.each(movies, function(i, movie) {
+                $moviesList.append('<tr><td class="left"><h4>' + movie.Title + '</h4></td><td><h4>' + movie.Genre + '</h4></td><td><h4>' + movie.Director + '</h4></td>')
+                });
+         }
+     });
+ });
